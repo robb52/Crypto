fce13defd48b7f6bd6cc513442d5e3da07ce71a5
Rob Brown <brown4homes@yahoo.com>
2018-11-01 15:25:19 -0700

    Added pricing data code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,16 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
+
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+
   end
 
   def prices
-
+  
   end
   
 end
